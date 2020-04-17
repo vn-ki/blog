d5e3cae66675438b54fb371b722d7ea58199a89b
Vishnunarayan K I <appukuttancr@gmail.com>
2020-04-17 12:34:49 +0530

    better toggle pos

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,20 +6,50 @@ import { ThemeToggler } from 'gatsby-plugin-dark-mode'
 import sun from "./images/sun.png"
 import moon from "./images/moon.png"
 
-
 import { rhythm, scale } from "../utils/typography"
 
 const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
   let header
 
+
+  let flexThemeToggle = (size) => (<ThemeToggler>
+    {({ theme, toggleTheme }) => (
+      <label
+        onClick={_ => toggleTheme(theme === 'light' ? 'dark' : 'light')}
+        style={{
+          fontFamily: `monospace`,
+          marginTop: `auto`,
+          marginBottom: `auto`,
+          height: rhythm(size),
+          width: rhythm(size),
+        }}
+      >
+        {console.log(theme)}
+        {theme === 'dark' ? (
+          <img style={{margin: `0px`, height: rhythm(size), width: rhythm(size)}} src={sun} alt="Light mode" />
+          //<p>0xfff</p>
+        ) : (
+          <img style={{margin: `0px`, height: rhythm(size), width: rhythm(size)}} src={moon} alt="Light mode" />
+          //<p>0x000</p>
+        )}
+
+      </label>
+    )}
+  </ThemeToggler>)
+
   if (location.pathname === rootPath) {
     header = (
-      <div>
+      <div style={{
+        display: `flex`,
+        justifyContent: `space-between`,
+        marginBottom: rhythm(1.5),
+        marginTop: 0,
+        }}>
         <h1
           style={{
             ...scale(1.5),
-            marginBottom: rhythm(1.5),
+            marginBottom: 0,
             marginTop: 0,
           }}
           className="header-link"
@@ -36,28 +66,37 @@ const Layout = ({ location, title, children }) => {
             {title}
           </Link>
         </h1>
+        {flexThemeToggle(1.5)}
       </div>
     )
   } else {
     header = (
-      <h3
-        style={{
-          fontFamily: `Montserrat, sans-serif`,
-          marginTop: 0,
-        }}
-        className="header-link-2"
-      >
-        <Link
+      <div style={{
+        display: `flex`,
+        margin: '0px',
+        }}>
+        <h3
           style={{
-            boxShadow: `none`,
-            color: `inherit`,
+            fontFamily: `Montserrat, sans-serif`,
+            display: `inline`,
+            marginTop: 0,
+            marginBottom: 0,
           }}
           className="header-link-2"
-          to={`/`}
         >
-          {title}
-        </Link>
-      </h3>
+          <Link
+            style={{
+              boxShadow: `none`,
+              color: `inherit`,
+            }}
+            className="header-link-2"
+            to={`/`}
+          >
+            {title}
+          </Link>
+        </h3>
+        {flexThemeToggle(1)}
+      </div>
     )
   }
   let theme_toggle = (<ThemeToggler>
@@ -71,6 +110,7 @@ const Layout = ({ location, title, children }) => {
           right: `10px`
         }}
       >
+        {console.log(theme)}
         {theme === 'dark' ? (
           <img width="40px"src={sun} alt="Light mode" />
             //<p>0xfff</p>
@@ -91,7 +131,7 @@ const Layout = ({ location, title, children }) => {
         padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
       }}
     >
-      <header>{header} {theme_toggle}</header>
+      <header>{header} {}</header>
       <main>{children}</main>
     </div>
   )
